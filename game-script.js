29b3ebbc015d911263d3c7cd969e8bf92c1a87e7
Paul Tillery IV <paultilleryiv@gmail.com>
2021-12-07 08:42:56 -0500

    12/7/21: refactored w/ a simple UI (buttons, text)

--- game-script.js
+++ game-script.js
@@ -1,60 +1,53 @@
-//Notes:
-//TOP said to show results with console.log(); I opted for alert/prompt/confirm instead
-//If TOP says to add GUI later, just keep that diff. in mind if they bring console.log back up
+//Should be working:
 
-let roundNumber;
-let playerScore;
-let computerScore;
+//Options for adding event listeners to buttons:
+    //Easy: use ID querySelectors to add each one individually
+    //More Elegant: use a "btn" class and querySelectorAll to add all at once, work with e variable
 
-document.addEventListener("load", game());
+//Options for using the playerSelection in PlayRound
+    //Easy: if each button has its own eventListener, they can each have their own function parametery
+    //More elegant: if using a nodeList and forEach, playerSelection can come from a property in e
 
-function game() {
-    playerScore = 0;
-    computerScore = 0;
-    
-    for (let i = 0; i <= 5; i++) {
-        
-        if (i < 5) {
-            //Prompt text
-            roundNumber = i + 1;
-            let promptText = "Round " + roundNumber + " of 5: Enter rock, paper, or scissors.";
+//Updates for new approach to rounds & scores:
+    //No more "# of 5," but still track roundNumber 
+    //Recap updates:
+        //If player score is 5, display win prompt
+        //Else if computer score is 5, display lose message
+        //Else recap scores, ask to play again
+    //Note: opting to use a prompt for win/lose, rather than adding a replay button
 
-            //Alert spawns prompt
-            alert( playRound( prompt(promptText), computerPlay() ) );
-        } else {
-            let winnerText;
+//Get buttons, give event listeners, have them play 
 
-            if (playerScore === computerScore) winnerText = "It's a tie!";
-            else if (playerScore > computerScore) winnerText = "You win!";
-            else winnerText = "The computer wins.";
-                
-            //Check on line breaks here
-            let gameRecap =
-                "That's game! Recap:" + "\n" +
-                scoreRecap() + "\n" +
-                winnerText + "\n\n" +
-                "Play again?";
-            
-            if (confirm(gameRecap)) game();
-            else alert("Thanks for playing!");
-        }
-        
-    }
-}
+let roundNumber = 0;
+let playerScore = 0;
+let computerScore = 0;
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => button.addEventListener("click", function(e) {
+        //console.log(e);
+        //console.log(e.target.id);
+        playRound(e.target.id, computerPlay() );
+        //e.stopPropagation();
+    }));
+
+const playerScoreText = document.querySelector("#player-score-text");
+const computerScoreText = document.querySelector("#computer-score-text");
+const recapText = document.querySelector("#recap-text");
+const nextPrompt = document.querySelector("#next-prompt");
 
 //Function for a single round.
 function playRound(playerSelection, computerSelection) {
-
-    //Decapitalize for evaluation
-    playerSelection = playerSelection.toLowerCase();
     
+    roundNumber ++;
+
     //Display who played what
     let roundRecap = 
-        "Round " + roundNumber + " of 5: " + 
+        "Round " + roundNumber + ": " + 
         "you played " + playerSelection + "; " + 
         "the computer played " + computerSelection + ".";
 
-    let roundResult;
+    let roundResult = "";
 
     //Set outcome scores & text
     if (playerSelection === computerSelection) {
@@ -65,17 +58,32 @@ function playRound(playerSelection, computerSelection) {
     playerSelection === "paper" && computerSelection === "rock" ||
     playerSelection === "scissors" && computerSelection === "paper") {
         playerScore ++;
+        playerScoreText.textContent = "Player Score: " + playerScore;
         roundResult = "You win this round!";
     }
     else {
         computerScore ++;
+        computerScoreText.textContent = "Computer Score: " + computerScore;
         roundResult = "You lose this round.";
     }
 
-    //Separate function for use in final recap
-    //let scoreRecap = "Player: " + playerScore + "\n" + "Computer: " + computerScore;
+    //Show recap; update play prompt after first round
+    recapText.textContent = roundRecap + "\n" + roundResult;
+    if (roundNumber === 1) {
+        nextPrompt.textContent = "Click below to play again!";
+    }
 
-    return roundRecap + "\n" + roundResult + "\n" + scoreRecap();
+    if (playerScore === 5) {
+        nextPrompt.textContent = "You won the game!";
+        alert("You won the game!");
+        location.reload();
+    }
+  
+    if (computerScore === 5) {
+        nextPrompt.textContent = "The computer won the game!";
+        alert("The computer won the game!");
+        location.reload();
+    }
 }
 
 function computerPlay() {
@@ -84,8 +92,4 @@ function computerPlay() {
     if (rng === 0) return "rock";
     else if (rng === 1) return "paper";
     else return "scissors";
-}
-
-function scoreRecap() {
-    return "Player: " + playerScore + "\n" + "Computer: " + computerScore;
 }
